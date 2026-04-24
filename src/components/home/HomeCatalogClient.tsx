e45a483a8135e5173e00fab405bade22331dc255
Moecharaf <moe.charaf110@gmail.com>
2026-04-25 02:55:34 +0300

    Use Acrylic banner image for rigid category hero

--- src/components/home/HomeCatalogClient.tsx
+++ src/components/home/HomeCatalogClient.tsx
@@ -232,7 +232,7 @@ const CATEGORY_ICON: Record<HomeCatalogSection["key"], string> = {
 
 const HERO_IMAGE_OVERRIDE: Partial<Record<HomeCatalogSection["key"], string>> = {
   banner: "/card-images/HD_Banner2.jpeg",
-  rigid: "/card-images/Acrlyic_Banner.jpeg",
+  rigid: "/card-images/Acry Banner .png",
   adhesive: "/card-images/CLING_Banner.jpeg",
   magnet: "/card-images/Magnets_Banner.jpeg",
 };
