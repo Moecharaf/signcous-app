849693f4fd25eb3ab8e6398f8dc24f228e9c46ea
Moecharaf <moe.charaf110@gmail.com>
2026-06-07 00:12:36 +0300

    Rename vinyl banner page label to HD banner

--- src/app/banners/vinyl-banner/page.tsx
+++ src/app/banners/vinyl-banner/page.tsx
@@ -9,6 +9,6 @@ export const metadata: Metadata = {
 
 export default function VinylBannerPage() {
   return (
-    <VinylBannerBuilder />
+    <VinylBannerBuilder productName="HD Banner ( Vinyl )" />
   );
 }
